a2f9ec34a354333ea89b1cd587f51d40e2f19bc0
tjsdn3065 <tjsdn3065@gmail.com>
2024-10-01 15:55:58 +0900

    문자열 출력 함수 추가

--- Common/utils.py
+++ Common/utils.py
@@ -18,3 +18,10 @@ def print_matInfo(name,image):
 
     print("%12s: depth(%s), channels(%s) -> mat_type(%sC%d)"
           % (name,image.dtype,nchannel,mat_type,nchannel))
+
+def put_string(frame,text,pt,value,color=(120,200,90)):
+    text += str(value)
+    shade=(pt[0]+2,pt[1]+2)
+    font=cv2.FONT_HERSHEY_SIMPLEX
+    cv2.putText(frame,text,shade,font,0.7,(0,0,0),2)
+    cv2.putText(frame,text,pt,font,0.7,color,2)
