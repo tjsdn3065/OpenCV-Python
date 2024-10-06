166e3d39d8c5e39bcb79b4cea2fea34b48a6818d
tjsdn3065 <tjsdn3065@gmail.com>
2024-10-06 15:17:13 +0900

    히스토그램 그리기 함수 추가

--- Common/utils.py
+++ Common/utils.py
@@ -25,3 +25,15 @@ def put_string(frame,text,pt,value,color=(120,200,90)):
     font=cv2.FONT_HERSHEY_SIMPLEX
     cv2.putText(frame,text,shade,font,0.7,(0,0,0),2)
     cv2.putText(frame,text,pt,font,0.7,color,2)
+
+def draw_histo(hist,shape=(200,256)):
+    hist_img = np.full(shape,255,np.uint8)
+    cv2.normalize(hist,hist,0,shape[0],cv2.NORM_MINMAX)
+    gap=hist_img.shape[1]/hist.shape[0]
+
+    for i,h in enumerate(hist):
+        x=int(round(i*gap))
+        w=int(round(gap))
+        cv2.rectangle(hist_img,(x,0,w,int(h)),0,cv2.FILLED)
+
+    return cv2.flip(hist_img,0)
